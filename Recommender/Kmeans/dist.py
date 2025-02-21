906817993a971863bcc681c7bf41eb277d125b08
mo992 <bookoob990@gmail.com>
2025-02-21 11:14:48 +0800

    Edit comments

--- Recommender/Kmeans/dist.py
+++ Recommender/Kmeans/dist.py
@@ -1,12 +1,10 @@
 import enum
 import numpy as np
 
-#列舉距離函數
 class distEnum(enum.Enum):
   cos = 0
   EuclideanDistance = 1
 
-# 距離函數
 class distance:
 
   def __init__(self, distFunctionName):
@@ -19,13 +17,11 @@ class distance:
       return 1 - np.dot(sample, center)/(np.dot(sample, sample)**(1/2)*np.dot(center, center)**(1/2))
 
   def EuclideanDistance(self, sample, center):
-    # 計算歐氏距離
     dist = 0
     for index in range(len(sample)):
       dist += (sample[index] - center[index])**2
     return np.sqrt(dist)
 
-  # 挑選距離函數
   def getDistFunc(self):
     distDict = {distEnum.cos: self.cosFunc,
                 distEnum.EuclideanDistance: self.EuclideanDistance}
